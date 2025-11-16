23f2e1f9d3fa2f4cf1b13cc97f721369bcfa8959
madara uchiha <notwhiten444@gmail.com>
2025-11-16 11:37:12 +0530

    -

--- dice.java
+++ dice.java
@@ -1,5 +1,15 @@
 public class dice {
    public static void main(String[] args) {
-    
+    dice("",4);
    } 
+   static void dice(String s,int no){
+    if(no==0){
+        System.out.println(s);
+        return;
+    }
+    for(int i=1 ; i<=6 && i<=no;i++){
+        dice(s+i,no-i);
+    }
+   }
+
 }
