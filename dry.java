d8d255d37fab7f69660dff4d78d7f46ec2ed3e9f
madara uchiha <notwhiten444@gmail.com>
2025-11-16 14:41:29 +0530

    -

--- dry.java
+++ dry.java
@@ -1,58 +1,21 @@
-
-
-public class dry {
+class dry {
     public static void main(String[] args) {
-int[] arr = {2,1,10};
-
-System.out.println(longestMonotonicSubarray(arr));
-
+        System.out.println(canPartition(new int[] {1,5,11,5}));
     }
-    static int longestMonotonicSubarray(int[] nums) {
-        int i =0 ;
-        int j = 0 ;
-        int max = 0; 
-        if(nums.length<=2){
-            if(nums.length==2){
-                if(nums[0]==nums[1]){
-                    return 1;
-                }
+    static boolean canPartition(int[] nums) {
+        int max=nums[0];
+        int ind = 0;
+        int count=0;
+        for(int i=0; i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+                ind=i;
             }
-        return nums.length;
-        } 
-        while(i<nums.length-1){
-            
-            if(nums[i+1]>nums[i]){
-                j++;
-            }
-            
-            else
-            {
-               j=0; 
-            }
-            if(j>max){
-                max=j;
-            }
-            i++;
         }
-        i=1;
-        j=0;
-        while(i<nums.length){
-            
-            if(nums[i-1]>nums[i]){
-                j++;
-            }
-            
-            else
-            {
-               j=0; 
-            }
-            if(j>max){
-                max=j;
-            }
-            i++;
-        }
-
-    return max+1;}
+        
+        for (int i=0; i<nums.length ;i++){
+            if(i!=ind){
+            count+=nums[i];}
+        }System.out.println(ind);
+    return count==max;}
 }
-
-
